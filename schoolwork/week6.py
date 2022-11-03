cb2fb20249463e8f13f59d5d32ebc37e5501faa7
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2022-11-04 03:28:04 +0800

    not yet

--- schoolwork/week6.py
+++ schoolwork/week6.py
@@ -9,44 +9,53 @@ for i in range(noi):                # load instructions to memeory
 while True:                         # machine cycle 機器循環
     ir=memory[pc]                   # 指令抓取 fetch instruction
     opcode=ir>>12                   # 解碼 取得op code 
-    Rd=R=(ir<<4)>>8                        # 取得 Rd 目的地暫存器 ,Rs1 來源暫存器1 ,Rs2 來源暫存器2
-    Rs1=(ir<<8)>>4
-    Rs2=ir<<12
-    Md=(ir<<4)>>4                                #      Ms 來源記憶體位址, Md 目的地記憶體位址
-    Ms=ir<<12
-                                    # pc 指向下一指令所在位址
-    if opcode==0:                   # 解碼執行指令碼 (參考指令表)
+    Rd = (ir>>8)%(2**4)             # 取得 Rd 目的地暫存器 ,Rs1 來源暫存器1 ,Rs2 來源暫存器2
+    Rs1 = (ir>>4)%(2**4)
+    Rs2 = (ir)%(2**4)
+    Md = (ir>>4)%(2**8)              # Ms 來源記憶體位址, Md 目的地記憶體位址
+    Ms = (ir)%(2**8)
+    pc = pc + 1                      # pc 指向下一指令所在位址
+    if opcode==0:                    # 解碼執行指令碼 (參考指令表)
         break
     elif opcode==1:
-        Rd=Ms
+        if Ms < 254:
+            register[Rd] = memory[Ms]
+        else:
+            register[Rd] = int(input())
     elif opcode==2:
-        Rs=ir<<12
-        Rd=Rs1+Rs2
+        if Ms < 254:
+            memory[Md] = register[Rs2]
+        else:
+            print(f"{register[Rs2]:X}")    
     elif opcode==3:
-        Rd=Rs1+Rs2
+        register[Rd] = register[Rs1] + register[Rs2]
     elif opcode==4:
-        Rd=Rs
+        register[Rd] = register[Rs1] + register[Rs2]
     elif opcode==5:
-        pass
+        register[Rd] = register[Rs1]
     elif opcode==6:
-        pass
+        register[Rd] = ((0xFFFF-register[Rs1]))
     elif opcode==7:
-        pass
+        register[Rd] = register[Rs1] & register[Rs2]
     elif opcode==8:
-        pass
+        register[Rd] = register[Rs1] | register[Rs2]
     elif opcode==9:
-        pass
+        register[Rd] = register[Rs1] ^ register[Rs2]
     elif opcode==10:
-        R+=1
+        register[Rd] = register[Rd]+1
     elif opcode==11:
-        R-=1
+        register[Rd] = register[Rd]-1
     elif opcode==12:
-        pass
+        if Rs2 == 0:
+            register[Rd] = register[Rd]
+        elif Rs2 == 1:
+            register[Rd] = register[Rd]
     elif opcode==13:
-        pass
+        if register[0]!= register[Rd]:
+            pc = Ms
     elif opcode==14:
-        pass
+        register[Rd] = int(input())
     elif opcode==15:
-        pass
+        print(f"{register[Rs2]:X}") 
     else:
-        break
+        break 
