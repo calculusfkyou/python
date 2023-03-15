dec587dedc5c2e6dc019a743ebfa69f869480863
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-03-15 21:37:32 +0800

    PC

--- domjudge exercise/110-2DSME1PE/PB.py
+++ domjudge exercise/110-2DSME1PE/PB.py
@@ -1,4 +1,9 @@
-num=list(map(int,input().split()))
-round=list(map(int,input().split()))
-for i in range(1,len(round)):
-    
+while True:
+    try:
+        num=list(map(int,input().split()))
+        round=list(map(int,input().split()))
+        temp=[]
+        for i in range(1,len(round)):
+            
+    except:
+        break
--- domjudge exercise/110-2DSME1PE/PC.py
+++ domjudge exercise/110-2DSME1PE/PC.py
@@ -1,15 +1,25 @@
+import math
 while True:
-    n=int(input())
-    val=list(map(int,input().split()))
-    temp=0
-    for i in range(0,len(val)):
-        temp+=val[i]
-    if n==0:
+    try:
+        n=int(input())
+        if n==0:
+            break
+        val=list(map(int,input().split()))
+        val.sort()
+        # temp=0
+        # for i in range(0,len(val)):
+        #     temp+=val[i]  #總數量
+        group=n//2
+        print(group)  #能有幾組
+        # single=val[0]+val[-1]  #一組有多少
+        # for i in range(int(group)-1):
+        #     print(single,end=" ")
+        # print(single)
+        for i in range(group):
+            #print("%d %d"%(val[i],val[n-i-1]))
+            if i+1==group:
+                print(val[i]+val[n-i-1])
+            else:
+                print(val[i]+val[n-i-1],end=" ")
+    except EOFError:
         break
-    group=n/2
-    if group%1>0:
-        print(int(group))
-        while(temp>int(group)):
-            print(temp/int(group),end=" ")
-            temp/=int(group)
-        
