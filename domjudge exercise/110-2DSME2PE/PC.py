976c767ffc505d7384b95aedc2797b962bbc56f0
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-03-25 23:23:10 +0800

    PC

--- domjudge exercise/110-2DSME2PE/PC.py
+++ domjudge exercise/110-2DSME2PE/PC.py
@@ -1,42 +1,60 @@
-def bbsort(subnum,n):
-	for k in range(n):
-		for j in range(0,len(subnum)-1):
-			if subnum[j]>subnum[j+1]:
-				temp=subnum[j+1]
-				subnum[j+1]=subnum[j]
-				subnum[j]=temp
-	return subnum
-
+#TRUE
 def insertsort(subnum,n):
-	for i in range(1,n-1):
-		key=subnum[i]
-		subi=i
-		while key<subnum[subi-1] and subi!=0:
-			temp=subnum[subi]
-			subnum[subi]=subnum[subi-1]
-			subnum[subi-1]=subnum[subi]
-			subi-=1
-	return subnum
-			
+    if n==1:
+        for i in range(len(subnum)-1):
+            print(subnum[i],end=" ")
+        print(subnum[-1])
+        return None
+    for i in range(1,n):
+        for k in range(i,0,-1):
+            if subnum[k]>subnum[k-1]:
+                subnum[k],subnum[k-1]=subnum[k-1],subnum[k]
+                i-=1
+            else:
+                break
+    for i in range(len(subnum)-1):
+        print(subnum[i],end=" ")
+    print(subnum[-1])
+
+#TRUE    
+def bbsort(subnum,n):
+    for j in range(len(subnum)-1,0,-1):
+        if subnum[j]>subnum[j-1]:
+            subnum[j],subnum[j-1]=subnum[j-1],subnum[j]
+    for i in range(len(subnum)-1):
+        print(subnum[i],end=" ")
+    print(subnum[-1])
+
 def selectsort(subnum,n):
-	for i in range(n):
-		subi=i
-		for k in range(i+1,len(subnum)+1):
-			if subnum[k]<subnum[subi]:
-				subi=k
-			if subi!=k:
-				temp=subnum[i]
-				subnum[i]=subnum[subi]
-	return subnum
-    
+    temp=[]
+    l=len(subnum)
+    for i in range(n):
+        idx=subnum.index(max(subnum))
+        subnum[0],subnum[idx]=subnum[idx],subnum[0]
+        temp.append(subnum[0])
+        subnum.pop(0)
+    temp+=subnum
+    return temp
+
 while True:
     try:
-        M,N=list(map(int,input().split()))
-        nums=input().split()
+        M,N=map(int,input().split())
+        nums=list(map(int,input().split()))
+        nums2=nums[:]
+        nums3=nums[:]
+        ss=[]
+        print(1)
         for i in range(1,N+1):
-            print(i)
-            print(insertsort(nums,i))
-            print(bbsort(nums,i))
-            print(selectsort(nums,i))
+            insertsort(nums,i)
+        print(2)
+        for i in range(1,N+1):
+            bbsort(nums2,i)
+        print(3)
+        for i in range(1,N+1):
+            ss=selectsort(nums3,i)
+            for k in range(len(ss)-1):
+                print(ss[k],end=" ")
+            print(ss[-1])
+            nums3=ss
     except EOFError:
         break
