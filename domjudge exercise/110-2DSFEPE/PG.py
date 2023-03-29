2b046268d21340ccdb456f9bef9e2297f98e773b
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-03-29 09:27:31 +0800

    PG

--- domjudge exercise/110-2DSFEPE/PG.py
+++ domjudge exercise/110-2DSFEPE/PG.py
@@ -3,9 +3,15 @@ while True:
         s=input()
         if s=='':
             break
+        temp=[]
         for i in range(len(s)-1):
-            print(s[i]+s[i+1:]+s[:i],end=" ")
-        print(s[-1]+s[0:-1])
+            if s[i]+s[i+1:]+s[:i] not in temp:
+                print(s[i]+s[i+1:]+s[:i],end=" ")
+            temp.append(s[i]+s[i+1:]+s[:i])
+        if s[-1]+s[0:-1] not in temp:
+            print(s[-1]+s[0:-1])
+            temp.append(s[-1]+s[0:-1])
+        temp.clear()
     except EOFError:
         break
 
--- domjudge exercise/110-2DSFEPE/PI.py
+++ domjudge exercise/110-2DSFEPE/PI.py
@@ -1,16 +1,46 @@
-def binarysearch(subtemp,key,left,right,count):
+# def binarysearch(subtemp,key,left,right,count):
+#     if left<=right:
+#         mid=(left+right)//2
+#         print(mid)
+#         if key==subtemp[mid]:
+#             #count+=1
+#             return count
+#         elif key<subtemp[mid]:
+#             #count+=1
+#             return binarysearch(subtemp,key,left,mid-1,count+1)
+#         elif key>subtemp[mid]:
+#             #count+=1
+#             return binarysearch(subtemp,key,mid+1,right,count+1)
+#     else:
+#         return -1
+        
+# while True:
+#     try:
+#         M,N=map(int,input().split())
+#         K=int(input())
+#         if K<M or K>N:
+#             print(-1)
+#         else:
+#             temp=[]
+#             for i in range(M,N+1):
+#                 temp.append(i)
+#             print(binarysearch(temp,K,M,N,1))
+#     except EOFError:
+#         break
+
+def binarysearch(key,left,right,count):
     if left<=right:
         mid=(left+right)//2
-        print(mid)
-        if key==subtemp[mid]:
+        # print(mid)
+        if key==mid:
             #count+=1
             return count
-        elif key<subtemp[mid]:
+        elif key<mid:
             #count+=1
-            return binarysearch(subtemp,key,left,mid-1,count+1)
-        elif key>subtemp[mid]:
+            return binarysearch(key,left,mid-1,count+1)
+        elif key>mid:
             #count+=1
-            return binarysearch(subtemp,key,mid+1,right,count+1)
+            return binarysearch(key,mid+1,right,count+1)
     else:
         return -1
         
@@ -21,9 +51,9 @@ while True:
         if K<M or K>N:
             print(-1)
         else:
-            temp=[]
-            for i in range(M,N+1):
-                temp.append(i)
-            print(binarysearch(temp,K,M,N,1))
+            # temp=[]
+            # for i in range(M,N+1):
+            #     temp.append(i)
+            print(binarysearch(K,0,N-M,1))
     except EOFError:
         break
