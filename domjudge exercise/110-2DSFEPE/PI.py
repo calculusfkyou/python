02f7cdb9191b26aea1a7d889b7dfce4c8fed48b8
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-05-04 11:30:19 +0800

    PI

--- domjudge exercise/110-2DSFEPE/PI.py
+++ domjudge exercise/110-2DSFEPE/PI.py
@@ -30,34 +30,34 @@
 
 #mine
 #fail
-def binarysearch(key,left,right,count):
-    if left<=right:
-        mid=(left+right)//2
-        if key==mid:
-            #count+=1
-            return count
-        elif key<mid:
-            #count+=1
-            return binarysearch(key,left,mid,count+1)
-        elif key>mid:
-            #count+=1
-            return binarysearch(key,mid,right,count+1)
-    else:
-        return -1
+# def binarysearch(key,left,right,count):
+#     if left<=right:
+#         mid=(left+right)//2
+#         if key==mid:
+#             #count+=1
+#             return count
+#         elif key<mid:
+#             #count+=1
+#             return binarysearch(key,left,mid,count+1)
+#         elif key>mid:
+#             #count+=1
+#             return binarysearch(key,mid,right,count+1)
+#     else:
+#         return -1
         
-while True:
-    try:
-        M,N=map(int,input().split())
-        K=int(input())
-        if K<M or K>N:
-            print(-1)
-        else:
-            # temp=[]
-            # for i in range(M,N+1):
-            #     temp.append(i)
-            print(binarysearch(K,M,N,1))
-    except EOFError:
-        break
+# while True:
+#     try:
+#         M,N=map(int,input().split())
+#         K=int(input())
+#         if K<M or K>N:
+#             print(-1)
+#         else:
+#             # temp=[]
+#             # for i in range(M,N+1):
+#             #     temp.append(i)
+#             print(binarysearch(K,M,N,1))
+#     except EOFError:
+#         break
 
 #GPT
 #correct
