9ceb05b9b531c3d2cac4864c539b5ae9f90b60ab
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-05-29 15:19:16 +0800

    test

--- test.py
+++ test.py
@@ -3,10 +3,14 @@
 #average = total /3
 #print('total = %d, and average = %.2f' %(total, average))
 
-from cmath import pi
-import math
-radius = eval(input('Enter the radius:'))
-area = radius * radius * math.pi
-perimeter = 2 * math.pi * radius
-print('Area = %.2f' %(area))
-print('Perimeter = %.2f'%(perimeter))
+# from cmath import pi
+# import math
+# radius = eval(input('Enter the radius:'))
+# area = radius * radius * math.pi
+# perimeter = 2 * math.pi * radius
+# print('Area = %.2f' %(area))
+# print('Perimeter = %.2f'%(perimeter))
+x=input()
+int(x,16)
+z=x.count('1')
+print(z)
