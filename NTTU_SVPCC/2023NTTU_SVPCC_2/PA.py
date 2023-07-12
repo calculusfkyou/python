0ba66b503a48d5a3704579214a3669e39e2c04b6
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-07-12 23:54:58 +0800

    PA.py

--- NTTU_SVPCC/2023NTTU_SVPCC_2/PA.py
+++ NTTU_SVPCC/2023NTTU_SVPCC_2/PA.py
@@ -1,8 +1,31 @@
 while True:
     try:
         n=int(input())
-        char=input().split()
-        val=input().split()
-
+        char=list(input().split())
+        val=list(input().split())
+        temp=[]
+        for i in range(len(val)):
+            if val[i]!="+" and val[i]!="-" and val[i]!="*":
+                temp.append(char[ord(val[i])-65])
+            elif val[i]=="-":
+                if temp[-1]=="T":
+                    temp.append("F")
+                else:
+                    temp.append("T")
+                temp.pop(-2)
+            elif val[i]=="*" or val[i]=="+":
+                if val[i]=="*":
+                    if temp[-1]=="T" and temp[-2]=="T":
+                        temp[-2]="T"
+                    else:
+                        temp[-2]="F"
+                    temp.pop()
+                else:
+                    if temp[-1]=="F" and temp[-2]=="F":
+                        temp[-2]="F"
+                    else:
+                        temp[-2]="T"
+                    temp.pop()
+        print(temp[0])
     except EOFError:
         break
---
+++ NTTU_SVPCC/2023NTTU_SVPCC_2/PB.py
@@ -0,0 +1,5 @@
+while True:
+    try:
+        m,n=map(int, input().split())
+    except EOFError:
+        break
