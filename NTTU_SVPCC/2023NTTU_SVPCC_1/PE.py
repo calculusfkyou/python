94e686cef76800ebc9e47d849bd61465453f4e82
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-07-13 22:29:38 +0800

    not yet

---
+++ NTTU_SVPCC/2023NTTU_SVPCC_1/PE.py
@@ -0,0 +1,10 @@
+while True:
+    try:
+        n,k=map(int,input().split())
+        nums=[]
+        for i in range(n):
+            nums.append(int(input()))
+        temp,ans=[],[]
+        
+    except EOFError:
+        break
