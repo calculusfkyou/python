36ae6ee6842f070004ee95d26b2b97e7b2e632f4
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-07-13 23:48:27 +0800

    not yet

--- NTTU_SVPCC/2023NTTU_SVPCC_7/PJ.py
+++ NTTU_SVPCC/2023NTTU_SVPCC_7/PJ.py
@@ -1,17 +1,18 @@
-while True:
-    try:
-        c,e,m=map(int,input().split())
-        ans=[]
-        for i in range(2,m):
-            if m%i==0:
-                if e==(i+m//i)*2:
-                    if (i+2)>(m//i+2):
+import random
+def main():
+    while True:
+        try:
+            c,e,m=map(int,input().split())
+            ans=[]
+            for i in range(2,m):
+                if m%i==0:
+                    if e==(i+m//i)*2:
                         ans.append([i+2,m//i+2])
-                    else:
-                        ans.append([m//i+2,i+2])
-        if len(ans)==0:
-            print('impossible')
-        else:
-            print(*ans[0])
-    except EOFError:
-        break
+            # print(ans)
+            if len(ans)==0:
+                print('impossible')
+            else:
+                print(*random.choice(ans))
+        except EOFError:
+            break
+main()
