8e90c1ce5947124c281cefd920d38ccdebe99373
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-07-13 21:05:32 +0800

    not yet

--- NTTU_SVPCC/2023NTTU_SVPCC_2/PB.py
+++ NTTU_SVPCC/2023NTTU_SVPCC_2/PB.py
@@ -1,5 +1,20 @@
+import math
+def gcd(m, n):
+    a = m
+    b = n
+    if n > m:
+        m, n = n, m
+    while m % n:
+        k = n
+        n = m % n
+        m = k
+    a //= n
+    b //= n
+    return n
 while True:
     try:
         m,n=map(int, input().split())
+        print(math.gcd(m,n))
+        # print(gcd(m,n))
     except EOFError:
         break
