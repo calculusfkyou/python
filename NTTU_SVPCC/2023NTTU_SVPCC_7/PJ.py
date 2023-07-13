727bd4e469d6468f8cf2d8280514776a1dd6a653
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-07-14 03:11:49 +0800

    fk wa

--- NTTU_SVPCC/2023NTTU_SVPCC_7/PJ.py
+++ NTTU_SVPCC/2023NTTU_SVPCC_7/PJ.py
@@ -1,14 +1,47 @@
 import random
+def prime_factorization(n):
+    factors=[]
+    i=2
+    while i*i<=n:
+        if n%i:
+            i+=1
+        else:
+            n//=i
+            factors.append(i)
+    if n>1:
+        factors.append(n)
+    return factors
+def get_factors(n):
+    factors=[]
+    # 質因數分解
+    for i in range(2,int(n**0.5)+1):
+        while n%i==0:
+            factors.append(i)
+            n//=i
+    if n>1:
+        factors.append(n)
+    # print(factors)
+    # 求得所有因數
+    num_factors=len(factors)
+    all_factors=set()
+    for i in range(1<<num_factors):
+        factor=1
+        for j in range(num_factors):
+            if (i>>j)&1:
+                factor*=factors[j]
+        all_factors.add(factor)
+    return sorted(all_factors)
 def main():
     while True:
         try:
             c,e,m=map(int,input().split())
             ans=[]
-            for i in range(2,m):
-                if m%i==0:
-                    if e==(i+m//i)*2:
-                        ans.append([i+2,m//i+2])
-            # print(ans)
+            temp=get_factors(m)
+            for i in range(len(temp)):  
+                t=m//temp[i]
+                if e==(t+temp[i])*2:
+                    ans.append([t+2,temp[i]+2])
+            print(ans)
             if len(ans)==0:
                 print('impossible')
             else:
--- test.py
+++ test.py
@@ -79,3 +79,29 @@
 # 4 6
 # 5 7
 # 6 7  
+def get_factors(n):
+    factors = []
+    # 質因數分解
+    for i in range(2, int(n**0.5) + 1):
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+    if n > 1:
+        factors.append(n)
+    print(factors)
+    # 求得所有因數
+    num_factors = len(factors)
+    all_factors = set()
+    for i in range(1 << num_factors):
+        factor = 1
+        for j in range(num_factors):
+            if (i >> j) & 1:
+                factor *= factors[j]
+        all_factors.add(factor)
+    
+    return sorted(all_factors)
+
+# 測試
+num = 36
+factors = get_factors(num)
+print(factors)
