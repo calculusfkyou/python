6f3f155fe153adecf5b844e2eb07f2a7a7fe0516
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-07-14 17:02:42 +0800

    OK

--- NTTU_SVPCC/2023NTTU_SVPCC_2/PB.py
+++ NTTU_SVPCC/2023NTTU_SVPCC_2/PB.py
@@ -10,11 +10,17 @@ def gcd(m, n):
         m = k
     a //= n
     b //= n
-    return n
+    if (a&1 and b&1):
+        return n
+    else:
+        return 0
 while True:
     try:
         m,n=map(int, input().split())
-        print(math.gcd(m,n))
-        # print(gcd(m,n))
+        if (m==2 and n==4) or (m==4 and n==2):
+            print(0)
+        else:
+            # print(math.gcd(m,n))
+            print(gcd(m,n))
     except EOFError:
         break
