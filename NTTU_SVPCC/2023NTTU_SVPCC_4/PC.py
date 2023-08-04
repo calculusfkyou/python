0b5cd28db0ff6d02cbc99adabce2f3101e921d60
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-08-04 23:29:04 +0800

    test

--- NTTU_SVPCC/2023NTTU_SVPCC_4/PC.py
+++ NTTU_SVPCC/2023NTTU_SVPCC_4/PC.py
@@ -1,6 +1,11 @@
+import math
+
 while True:
     try:
         w,h=map(int,input().split())
-        print(w+h-(float(w**2+h**2))**0.5)
+        if w+h-math.sqrt(w**2+h**2)-int(w+h-math.sqrt(w**2+h**2))!=0:
+            print("%.9f"%(w+h-math.sqrt(w**2+h**2)))
+        else:
+            print("%d"%(w+h-math.sqrt(w**2+h**2)))
     except EOFError:
         break
