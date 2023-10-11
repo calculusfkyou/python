e6e7b20f7793e4eb8f10ceab61d8edce4565440e
calculusfkyou <114671920+calculusfkyou@users.noreply.github.com>
2023-10-12 00:11:56 +0800

    re

--- python_module/Os.py
+++ python_module/Os.py
@@ -7,9 +7,9 @@ print("當前工作目錄：", current_directory)
 
 # os.remove(filename)
 # 用於刪除指定的文件。它需要文件名作為參數，並將該文件刪除。
-# file_to_delete = "tt.py"
-# os.remove(file_to_delete)
-# print(f"文件 '{file_to_delete}' 已刪除")
+file_to_delete = "tt.py"
+os.remove(file_to_delete)
+print(f"文件 '{file_to_delete}' 已刪除")
 
 # os.mkdir(directory)
 # 用於創建新的目錄。它需要一個目錄名作為參數，並創建一個新的目錄，名稱由參數指定。
@@ -29,15 +29,15 @@ print(os.path.exists("new_folder"))
 
 # os.makedirs(name, mode=0o777, exist_ok=False)
 # 用於創建多層目錄，包括所有中間目錄。它需要一個目錄名作為參數。您可以使用 mode 參數指定創建目錄的權限，exist_ok 參數用於確定如果目錄已經存在時是否引發異常。
-# nested_directory = "parent/child/grandchild"
-# os.makedirs(nested_directory, exist_ok=True)
-# print(f"已創建多層目錄 '{nested_directory}'")
+nested_directory = "parent/child/grandchild"
+os.makedirs(nested_directory, exist_ok=True)
+print(f"已創建多層目錄 '{nested_directory}'")
 
 import shutil
 
 # rmtree
 # 這個函數允許您刪除整個目錄，包括它的所有子目錄和文件。
 # 刪除指定目錄(非空目錄)
-# shutil.rmtree("parent/child/grandchild")
-# shutil.rmtree("parent/child")
-# shutil.rmtree("parent")
+shutil.rmtree("parent/child/grandchild")
+shutil.rmtree("parent/child")
+shutil.rmtree("parent")
